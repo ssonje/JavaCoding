e54ac86d0f42c2411761138b456972d8f892ae18
ssonje <ssonje99@gmail.com>
2023-02-02 19:56:14 +0530

    Swap Two Numbers

---
+++ JavaCoding/src/SwapTwoNumbers.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class SwapTwoNumbers {
+
+    private static void swapNumbers(int a, int b) {
+        System.out.println("Before swapping a = " + a + " and b = " + b);
+
+        // swap two numbers
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping a = " + a + " and b = " + b);
+    }
+
+    private static void swapNumbersWithoutUsingThirdVariable(int a, int b) {
+        System.out.println("Before swapping a = " + a + " and b = " + b);
+
+        // swap two numbers
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping a = " + a + " and b = " + b);
+    }
+
+    public static void main(String[] args) {
+        // Get integer input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter two Numbers - ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        // swap numbers using traditional way (third variable)
+        System.out.println("Swapping two numbers using third variable - ");
+        swapNumbers(a, b);
+
+        // swap numbers without using third variable
+        System.out.println("Swapping two numbers without using third variable - ");
+        swapNumbersWithoutUsingThirdVariable(a, b);
+    }
+
+}
