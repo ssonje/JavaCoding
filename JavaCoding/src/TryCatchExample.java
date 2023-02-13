17ff859ded7600f65069734212a2692739dd6874
ssonje <ssonje99@gmail.com>
2023-02-13 22:15:05 +0530

    Try Catch Example

---
+++ JavaCoding/src/TryCatchExample.java
@@ -0,0 +1,12 @@
+public class TryCatchExample {
+
+    public static void main(String[] args) {
+        try {
+            int a = 10/0;
+        } catch (ArithmeticException arithmeticException) {
+            System.out.println("ArithmeticException is catch.");
+            arithmeticException.printStackTrace();
+        }
+    }
+
+}
