908d70f3549f3dc107be61efecbb61caccd8fd38
ssonje <ssonje99@gmail.com>
2023-02-18 23:00:20 +0530

    Minor NIT

--- JavaCoding/src/FunctionalInterfaceExample.java
+++ JavaCoding/src/FunctionalInterfaceExample.java
@@ -2,21 +2,19 @@ import java.lang.*;
 
 @FunctionalInterface
 interface FunctionalInterfaceInterface {
-
     void sampleMethod();
-
 }
 
 public class FunctionalInterfaceExample implements FunctionalInterfaceInterface {
 
-    public static void main(String[] args) {
-        FunctionalInterfaceExample object = new FunctionalInterfaceExample();
-        object.sampleMethod();
-    }
-
     @Override
     public void sampleMethod() {
         System.out.println("Sample method.");
+    }
+
+    public static void main(String[] args) {
+        FunctionalInterfaceExample object = new FunctionalInterfaceExample();
+        object.sampleMethod();
     }
 }
 
