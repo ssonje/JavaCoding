7c8f8b58fd7d9487a5c4d90a909c3f3e2a76d36d
ssonje <ssonje99@gmail.com>
2023-03-12 22:49:40 +0530

    Fixed algo

--- JavaCoding/src/JavaPracticeCodingQuestions/StringToNumber.java
+++ JavaCoding/src/JavaPracticeCodingQuestions/StringToNumber.java
@@ -2,27 +2,16 @@ package JavaPracticeCodingQuestions;
 
 public class StringToNumber {
 
-    private static int reverseNumber(int number) {
-        int answer = 0;
-        while (number != 0) {
-            int rem = number % 10;
-            answer = answer * 10 + rem;
-            number /= 10;
-        }
-
-        return answer;
-    }
-
     private static int convertStringToNumber(String s) {
         s = s.toLowerCase();
         int number = 0;
         int length = s.length();
-        for (int i = length - 1; i >= 0; i--) {
+        for (int i = 0; i < length; i++) {
             int x = s.charAt(i) - '0';
             number = number * 10 + x;
         }
 
-        return reverseNumber(number);
+        return number;
     }
 
     public static void main(String[] args) {
